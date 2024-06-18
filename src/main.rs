f22dbd3ac25e05295311e8cbbfe20f245f967a35
jayden-dang <jayden@jaydendang.com>
2024-06-18 17:48:39 +0700

    feat: finish initial project

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,8 @@ async fn root() -> &'static str {
   "Hello, World!"
 }
 
-async fn create_user(
-  // this argument tells axum to parse the request body
-  // as JSON into a `CreateUser` type
-  Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-  // insert your application logic here
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
   let user = User { id: 1337, username: payload.username };
-
-  // this will be converted into a JSON response
-  // with a status code of `201 Created`
   (StatusCode::CREATED, Json(user))
 }
 
